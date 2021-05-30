2e9e9aa504c6e7648dc540127fdbeaca7c7a2122
a6d <a6d@mail.com>
2021-05-30 02:17:20 +0200

    v1.0.2 - J'essaie de faire marcher l'updater :ye:

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1234,7 +1234,7 @@ function populateAboutVersionInformation() {
  */
 function populateReleaseNotes() {
     $.ajax({
-        url: 'https://github.com/dscalzi/HeliosLauncher/releases.atom',
+        url: 'https://github.com/Craftok/Launcher/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
